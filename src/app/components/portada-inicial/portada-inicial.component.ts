a1b5f0007f98b54d762d09c21d819c6453ec9a47
AlonsoJIC <jalonsojic@gmail.com>
2023-08-26 01:21:36 -0600

    v0.2 -- Added components, header & photos.

---
+++ src/app/components/portada-inicial/portada-inicial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portada-inicial',
+  templateUrl: './portada-inicial.component.html',
+  styleUrls: ['./portada-inicial.component.css']
+})
+export class PortadaInicialComponent {
+
+}
